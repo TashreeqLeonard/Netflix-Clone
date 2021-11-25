df6cb6db860e7afd2c18ee660fdc7a592e99e567
TashreeqLeonard <88372663+TashreeqLeonard@users.noreply.github.com>
2021-11-25 09:58:34 +0200

    Added more styling and HTML

--- Js/main.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('#nav');
-
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >=100){
-        nav.classList.add('top')
-    }else{
-        nav.classList.remove('top')
-    }
-});
